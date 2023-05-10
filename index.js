ca398cf3f5ef98bc21a6d6d10ac67b8055242767
bewasoeryabagastara <bewasoerya77@gmail.com>
2023-05-10 20:14:06 +0700

    add some code after paragraf

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const index = () => {
   return (
     <div>index</div>
     <p>haloo dnia</p>
-  )
+    useLayoutEffect(() => {
+      first
+    
+      return () => {
+        second
+      };
+    }, [third])
 }
 
 export default index
